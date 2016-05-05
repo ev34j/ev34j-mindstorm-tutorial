8a67f539d3d6eca37fe7d2082cfe2297bbceb46d
Paul Ambrose <pambrose@mac.com>
2016-05-05 11:57:45 -0700

    Add comments

--- src/main/java/MotorUsage.java
+++ src/main/java/MotorUsage.java
@@ -1,12 +1,13 @@
 import com.ev34j.core.utils.Delay;
 import com.ev34j.mindstorm.motor.LargeMotor;
 import com.ev34j.mindstorm.motor.SteeringMotors;
+import com.ev34j.mindstorm.motor.TankMotors;
 
 public class MotorUsage {
 
   public static void main(final String[] args) {
 
-    // Run the motors as separate objects
+    // Run the motors as LargeMotor objects
     System.out.println("Trying out LargeMotors");
     LargeMotor lmA = new LargeMotor("A");
     LargeMotor lmB = new LargeMotor("B");
@@ -24,13 +25,28 @@ public class MotorUsage {
     lmA.waitUntilStopped();
     lmB.waitUntilStopped();
 
-
+    // Run the motors as a SteeringMotors object
     System.out.println("Trying out SteeringMotors");
     final SteeringMotors sm = new SteeringMotors("A", "B");
-    sm.onForRotations(2, 20, 25);
-    sm.waitUntilStopped();
-    sm.onForRotations(2, 20, -25);
+
+    // Go forward at a 45 degree angle
+    sm.onForRotations(2, 50, 25);
     sm.waitUntilStopped();
 
+    // Go back at a 45 degree angle
+    sm.onForRotations(2, 50, -25);
+    sm.waitUntilStopped();
+
+    // Run the motors as a TankMotors object
+    System.out.println("Trying out TankMotors");
+    final TankMotors tm = new TankMotors("A", "B");
+
+    // Go forward at a 45 degree angle
+    tm.onForRotations(2, 12, 25);
+    tm.waitUntilStopped();
+
+    // Go back at a 45 degree angle
+    tm.onForRotations(2, -12, -25);
+    tm.waitUntilStopped();
   }
 }
