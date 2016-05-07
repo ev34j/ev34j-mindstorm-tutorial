f7ea869bbf3dfc7418051abda46fc6ed58518a4c
Paul Ambrose <pambrose@mac.com>
2016-05-07 14:19:53 -0700

    Add ReadingMotorPositions

---
+++ src/main/java/ReadingMotorPositions.java
@@ -0,0 +1,25 @@
+import com.ev34j.core.utils.Delay;
+import com.ev34j.mindstorm.motor.MediumMotor;
+
+public class ReadingMotorPositions {
+
+  public static void main(String[] args) {
+
+    // Read the position value from a MediumMotor
+    // Output the value only when the value changes
+    // Stop after observing 10 new values
+    MediumMotor motor = new MediumMotor("D");
+
+    int count = 0;
+    int lastVal = -1;
+    while (count <= 10) {
+      int currVal = motor.getPosition();
+      if (currVal != lastVal) {
+        count++;
+        lastVal = currVal;
+        System.out.printf("Medium motor position: %d", lastVal);
+      }
+      Delay.delayMillis(100);
+    }
+  }
+}
