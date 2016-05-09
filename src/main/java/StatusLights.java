20ea8041d41a25fb9eb8e990c9b2dbc18a3ade8d
Paul Ambrose <pambrose@mac.com>
2016-05-08 22:13:06 -0700

    Add StatusLights

---
+++ src/main/java/StatusLights.java
@@ -0,0 +1,33 @@
+import com.ev34j.core.utils.Delay;
+import com.ev34j.mindstorm.leds.Ev3StatusLight;
+import com.ev34j.mindstorm.leds.Ev3StatusLights;
+import com.ev34j.mindstorm.sound.Ev3Sound;
+
+public class StatusLights {
+
+  public static void main(String[] args) {
+
+    Ev3StatusLight left = Ev3StatusLights.left();
+    Ev3StatusLight right = Ev3StatusLights.right();
+
+    Ev3Sound.sayAsEnglish("Red");
+    left.red();
+    right.red();
+    Delay.delaySecs(2);
+
+    Ev3Sound.sayAsEnglish("Green");
+    left.green();
+    right.green();
+    Delay.delaySecs(2);
+
+    Ev3Sound.sayAsEnglish("Orange");
+    left.orange();
+    right.orange();
+    Delay.delaySecs(2);
+
+    Ev3Sound.sayAsEnglish("Off");
+    left.off();
+    right.off();
+
+  }
+}
