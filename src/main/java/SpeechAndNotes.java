4644f11aedb0284c8f807c8379c8221db49f517a
Paul Ambrose <pambrose@mac.com>
2016-05-25 16:33:16 -0700

    Fix typo

--- src/main/java/SpeechAndNotes.java
+++ src/main/java/SpeechAndNotes.java
@@ -2,13 +2,11 @@ import com.ev34j.mindstorm.sound.Ev3Sound;
 import com.ev34j.mindstorm.sound.Note;
 import com.ev34j.mindstorm.time.Wait;
 
-public class SpeachAndNotes {
+public class SpeechAndNotes {
 
   public static void main(String[] args) {
 
     Ev3Sound.say("I am a LEGO robot", 100);
-    Wait.forSecs(1);
-    Ev3Sound.say("Soy un robot LEGO", 100);
     Wait.forSecs(1);
 
     Ev3Sound.playNote(Note.C4, 1, 100);
